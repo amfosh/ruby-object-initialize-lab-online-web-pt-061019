06e78c8b6cb887bdd340b2cc3743891eeb3d1c2d
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-25 23:16:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,8 @@
 class Dog 
   
-def initialize(name)
+def initialize(name, breed)
   @name = name
+  @breed = breed
 end
 
 def name=(name)
@@ -10,10 +11,6 @@ end
 
 def name
   @name
-end
-
-def initialize(breed)
-  @breed = breed
 end
 
 def breed=(breed)
